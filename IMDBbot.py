74b384f67a74f2f5430e170bae8f0edc49183dd3
Sichanak <94844596+Sichanak@users.noreply.github.com>
2024-10-15 02:06:22 +0330

    Add files via upload

---
+++ IMDBbot.py
@@ -0,0 +1,300 @@
+import os
+import logging
+from uuid import uuid4
+from datetime import datetime
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ParseMode
+from telegram.ext import Application, Updater, InlineQueryHandler, CommandHandler, CallbackQueryHandler, ChosenInlineResultHandler, MessageHandler, filters
+import movie
+from dotenv import load_dotenv
+
+
+
+# Global vars
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+DATABASE = '/storage/emulated/0/Download/IMDBbot/database/imdb_db.sqlite3'
+JOB_TIME = (9, 30) # time at which notifications are sent (UTC)
+
+
+# setup a simple logging
+logging.basicConfig(format='%(asctime)s - %(name)s - ' \
+                           '%(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+# Disable info level logging from below module as it spams
+logging.getLogger('imdb.parser.http.piculet').setLevel(logging.ERROR)
+
+LOG = logging.getLogger(__name__)
+
+
+def notify_users(context):
+    """
+    Notify users upon title release
+    """
+
+    alert = movie.Alert(DATABASE)
+    results = alert.notify()
+    for result in results:
+        user_id, message = result[0], result[1]
+        context.bot.send_message(chat_id=user_id,
+                                 text=message,
+                                 parse_mode=ParseMode.HTML)
+
+
+def result_id(title_id):
+    """
+    Generate UUID containig IMDb title ID
+    """
+
+    uuid4_str = str(uuid4())
+    my_uuid = uuid4_str + '-' + str(title_id)
+    return my_uuid
+
+
+async def help_cmd(update, context):
+    """
+    Reply with help message when the command /help is issued.
+    """
+
+    bot_info = await context.bot.getMe()
+    bot_name = bot_info.username
+    await update.message.reply_text(text='Search for a title by typing @{0} "movie name", '
+                                   'pick a result from the list and set an alert to '
+                                   'receive a notification when the movie or series '
+                                   'episode is out!\n\nType /alerts to view your act'
+                                   'ive alerts.'.format(bot_name))
+
+
+async def alerts_cmd(update, context):
+    """
+    Reply with list of enabled alerts when the command /alerts is issued
+    """
+
+    user_id = update.message.from_user.id
+    alert = movie.Alert(DATABASE)
+    message = alert.title_name(user_id)
+    await update.message.reply_html(message)
+
+
+async def unknown_cmd(update, context):
+    """
+    Unsupported command message handler
+    """
+
+    chatid = update.effective_chat.id
+    is_bot = update.effective_user.is_bot
+    if not is_bot:
+        await context.bot.send_message(chat_id=chatid, text='Unrecognized command, ' \
+                                                      'type /help or /alerts')
+
+
+async def chosen_result(update, context):
+    """
+    Get chosen inline result
+    """
+
+    result = update.chosen_inline_result
+    resultid = result.result_id
+    title_id = resultid.split('-')[-1]
+    user_id = result.from_user.id
+    # Store chosen result's title_id in context
+    context.user_data[user_id] = title_id
+
+
+def imdb_url_button(title_id, message):
+    """
+    After chosing enable/disable alert create IMDb URL button
+    """
+
+    imdb_url = 'https://www.imdb.com/title/tt' + str(title_id)
+    message = str(message) + ' (IMDb link)'
+    keyboard = [[InlineKeyboardButton(text=str(message),
+                                      url=imdb_url)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
+
+
+async def enable_alert(update, context):
+    """
+    Enable Alert for chosen inline result.
+    """
+
+    query = update.callback_query
+    query.answer(text='Searching release date...')
+    # Get user info
+    user = ['id', 'first_name', 'last_name', 'username']
+    user_info = [query.from_user[i] for i in user if query.from_user[i]]
+    user_name = ' '.join(user_info[1:])
+    # Retrieve chosen title
+    title_id = context.user_data[user_info[0]]
+    # Remove buttons and enable alert
+    query.edit_message_reply_markup(reply_markup=None)
+    alert = movie.Alert(DATABASE)
+    result = alert.enable(user_info[0], user_name, title_id)
+    # Respond with IMDb link button
+    new_reply_markup = imdb_url_button(title_id, result)
+    query.edit_message_reply_markup(reply_markup=new_reply_markup)
+
+
+async def disable_alert(update, context):
+    """
+    Disable alert for chosen inline result.
+    """
+
+    query = update.callback_query
+    query.answer()
+    user_id = query.from_user.id
+    title_id = context.user_data[user_id]
+    # disable alert
+    alert = movie.Alert(DATABASE)
+    result = alert.disable(user_id, title_id)
+    # send response as button
+    new_reply_markup = imdb_url_button(title_id, result)
+    query.edit_message_reply_markup(reply_markup=new_reply_markup)
+
+
+async def dismiss(update, context):
+    """
+    Dismiss chosen inline result
+    """
+
+    query = update.callback_query
+    query.answer()
+    query.edit_message_reply_markup(reply_markup=None)
+
+
+def create_reply_markup(title, current_year, user_titles):
+    """
+    Create reply markup for result based on title and user alerts
+    """
+    keyboard = [[InlineKeyboardButton("Enable alert",
+                                      callback_data=str(enable_alert)),
+                 InlineKeyboardButton("Disable alert",
+                                      callback_data=str(disable_alert)),
+                 InlineKeyboardButton("Dismiss",
+                                      callback_data=str(dismiss))]]
+
+    # Check if series has ended
+    if 'kind' in title and 'series' in title['kind']:
+        if 'end_year' in title and title['end_year']:
+            message = 'Series ended in {0}'.format(title['end_year'])
+            reply_markup = imdb_url_button(title['id'], message)
+            return reply_markup
+
+    # Check if movie was released
+    if 'year' in title:
+        if current_year > title['year']:
+            message = 'Movie released in {0}'.format(title['year'])
+            reply_markup = imdb_url_button(title['id'], message)
+            return reply_markup
+
+    # Remove enable/disable button based on user's existing alerts
+    if str(title['id']) in user_titles:
+        del keyboard[0][0]
+    else:
+        del keyboard[0][1]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
+
+
+async def in_line_query(update, context):
+    """
+    Handle the inline query.
+    """
+
+    query = update.inline_query.query
+    is_bot = update.inline_query.from_user.is_bot
+    if not is_bot:
+
+        current_year = int(datetime.now().strftime('%Y'))
+
+        # get user's alert title IDs
+        user_id = update.inline_query.from_user.id
+        user_titles = movie.Alert(DATABASE).title_id(user_id)
+
+        # search IMDb for titles
+        titles = movie.search(query)
+        results = []
+
+        for title in titles:
+
+            reply_markup = create_reply_markup(title, current_year, user_titles)
+
+            result = InlineQueryResultArticle(id=result_id(title['id']),
+                                              title=title['long imdb title'],
+                                              input_message_content=InputTextMessageContent(
+                                                  message_text=movie.reply_message(title),
+                                                  parse_mode=ParseMode.HTML),
+                                              description=title['plot'],
+                                              thumbnail_url=title['cover url'],
+                                              reply_markup=reply_markup)
+            results.append(result)
+
+        await update.inline_query.answer(results, cache_time=4)
+
+
+def log_error(update, context):
+    """
+    Log Errors caused by Updates.
+    """
+
+    LOG.error('Update "%s" caused error: "%s"', update, context.error)
+
+
+def main():
+    """
+    Create the updater and Application handlers
+    """
+    # Get the Application to register handlers
+    app = Application.builder().token(TOKEN).build()
+
+    # Create the bot's alert database
+    movie.Alert(DATABASE).create_db()
+
+    # Create the updater and pass the bot's token. 
+    #(old and new updater Commented so i can find out it usfull or not)
+    #updater = Updater(TOKEN, use_context=True, workers=32)
+    #updater = Updater(TOKEN, workers=32)
+    
+    # Create repeating job to notify users
+    job_start_time = datetime.time(datetime.now().replace(hour=int(JOB_TIME[0]),
+                                                          minute=int(JOB_TIME[1])))
+    job = app.job_queue
+    job.run_repeating(notify_users, interval=86400, first=job_start_time)
+
+    # On different commands - answer in Telegram
+    app.add_handler(CommandHandler("start", help_cmd))
+    app.add_handler(CommandHandler("help", help_cmd))
+    app.add_handler(CommandHandler("alerts", alerts_cmd))
+
+    # Add the inline query handler
+    app.add_handler(InlineQueryHandler(in_line_query))
+
+    # On chosing result, get its ID
+    app.add_handler(ChosenInlineResultHandler(chosen_result))
+
+    # On button selection call appropriate function
+    app.add_handler(CallbackQueryHandler(enable_alert, pattern='^'+str(enable_alert)+'$'))
+    app.add_handler(CallbackQueryHandler(disable_alert, pattern='^'+str(disable_alert)+'$'))
+    app.add_handler(CallbackQueryHandler(dismiss, pattern='^'+str(dismiss)+'$'))
+
+    # Answer to non-commands
+    app.add_handler(MessageHandler((~ filters.Entity('url')) &
+                                        (~ filters.Entity('text_link')), unknown_cmd))
+
+    # Log all errors
+    app.add_error_handler(log_error)
+
+    # Start the Bot
+    app.run_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # run_polling() is non-blocking and will stop the bot gracefully.
+    
+
+
+if __name__ == '__main__':
+    main()
--- movie.py
+++ movie.py
@@ -1,15 +1,16 @@
 import functools
 import logging
-from re import match
 from datetime import datetime, timedelta
-from imdb import IMDb
+from imdb import Cinemagoer
 import db
 
+# Global logger & vars
 LOG = logging.getLogger(__name__)
+ia = Cinemagoer()  # Initialize Cinemagoer
 
 def _catch_and_log(func):
     """
-    Decorator function for catching and logging exceptions
+    Decorator function for catching and logging exceptions.
     """
     @functools.wraps(func)
     def try_func(*args, **kwargs):
@@ -17,76 +18,85 @@ def _catch_and_log(func):
             return func(*args, **kwargs)
         except Exception as err:
             LOG.error('Exception in %s: "%s"', func.__qualname__, err)
-            return 'Unexpected error occured.'
+            return 'Unexpected error occurred.'
     return try_func
 
 @_catch_and_log
 def search(name):
     """
-    Search for titles matching name string and return a list of dictionaries
+    Search for titles matching the name string and return a list of dictionaries.
     """
-    imdb_results = IMDb().search_movie_advanced(name, results=10)
+    imdb_results = ia.search_movie(name)  # Search for movies by name
     titles = []
-    for result in imdb_results:
+
+    for result in imdb_results[:10]:  # Limit results to the first 10
         title_dict = get_fields(result)
         titles.append(title_dict)
+
     return titles
 
 @_catch_and_log
 def get_fields(imdb_data):
     """
-    Get the fields for telegram replies from the imdb result
-
-    (title, plot, year, end_year, genres, rating, kind, cast,
-     long imdb title, cover url and full-size cover url)
+    Extract fields for IMDb data (title, plot, year, genres, rating, etc.)
     """
     na_cover = 'https://i.imgur.com/A8SBkqe_d.jpg?maxwidth=640&shape=thumb&fidelity=medium'
-    title_id = imdb_data.getID()
-    fields = {'title':'N/A', 'plot':'N/A', 'end_year':None,
-              'year':None, 'genres':'N/A', 'rating':'N/A',
-              'kind':'N/A', 'cast':'N/A', 'long imdb title':'N/A',
-              'cover url':na_cover, 'full-size cover url':na_cover,
-              'series title':'N/A', 'season':'N/A', 'episode': 'N/A',
-              'id': title_id}
-    for field in fields:
-        value = imdb_data.get(field)
-        if value:
-            if isinstance(value, list):
-                if field == 'cast':
-                    value = ', '.join([n['name'] for n in value][0:4])
-                else:
-                    value = ', '.join(value).split('::')[0]
-            fields[field] = value
+    title_id = imdb_data.movieID  # Get IMDb ID
+
+    fields = {
+        'title': imdb_data.get('title', 'N/A'),
+        'year': imdb_data.get('year', 'N/A'),
+        'genres': ', '.join(imdb_data.get('genres', ['N/A'])),
+        'rating': imdb_data.get('rating', 'N/A'),
+        'plot': imdb_data.get('plot', ['N/A'])[0] if imdb_data.get('plot') else 'N/A',  # Safely get plot
+        'kind': imdb_data.get('kind', 'N/A'),
+        'cast': ', '.join([person['name'] for person in imdb_data.get('cast', [])[:4]]),
+        'long imdb title': imdb_data.get('long imdb canonical title', 'N/A'),
+        'cover url': imdb_data.get('cover url', na_cover),
+        'full-size cover url': imdb_data.get('full-size cover url', na_cover),
+        'id': title_id
+    }
+
+    # Optionally check for series-specific fields
+    if 'series' in fields['kind'].lower():
+        fields['series title'] = fields['title']  # Use existing title field
+        fields['season'] = imdb_data.get('season', 'N/A')
+        fields['episode'] = imdb_data.get('episode', 'N/A')
+
     return fields
 
 @_catch_and_log
 def reply_message(title):
     """
-    Telegram reply message for selected inline result
+    Telegram reply message for selected inline result.
     """
-    if title['kind'] == 'episode':
-        bold_fields = ['series title', 'title', 'plot', 'season', 'episode']
-    else:
-        bold_fields = ['title', 'genres', 'plot', 'rating', 'cast']
-    formated_fields = []
-    formated_cover = '< a href="{0}">&#8204;</a>'.format(title['full-size cover url'])
+    # movie or episode
+    bold_fields = ['series title', 'title', 'plot', 'season', 'episode'] if title.get('kind') == 'episode' else ['title', 'genres', 'plot', 'rating', 'cast']
+
+    formatted_fields = []
+    # Hide link for cover image so only the image appears
+    formatted_cover = '<a href="{0}">&#8204;</a>'.format(title['full-size cover url'])
+
     title_line = '<b>{0} ({1}) | {2}</b>\n\n'.format(title[bold_fields[0]], title['year'], title['kind'])
     for field in bold_fields[1:]:
-        line = '<b>{0}:</b> {1}'.format(field.title(), title[field])
-        formated_fields.append(line)
-    message = title_line + '\n'.join(formated_fields) + formated_cover
+        line = '<b>{0}:</b> {1}'.format(field.title(), title.get(field, 'N/A'))  # Use .get() for safety
+        formatted_fields.append(line)
+
+    message = title_line + '\n'.join(formatted_fields) + formatted_cover
+
     return message
 
-class Alert():
+class Alert:
     """
-    Enable/disable alerts for given IMDb title and return Telegram message
+    Enable/disable alerts for given IMDb title and return Telegram message.
     """
+
     def __init__(self, db_location):
         """
-        Initialize database and create alerts table
+        Initialize database and create alerts table.
         """
         self.db_api = db.Database(db_location)
-        self.imdb_api = IMDb()
+        self.imdb_api = ia  # Use Cinemagoer instance
 
     def __del__(self):
         self.db_api.close()
@@ -94,24 +104,34 @@ class Alert():
     @_catch_and_log
     def create_db(self):
         """
-        Create database and table
+        Create database and table.
         """
         self.db_api.create_table()
+
 
     @_catch_and_log
     def _get_movie_release_date(self, user_id, user_name, title_id, title_name):
         """
         Get movie release date and update database if not yet released
         """
+
         result = self.imdb_api.get_movie_release_info(title_id)
         release_dates = result['data'].get('raw release dates')
+        # regex to check release date string as to avoid datetime.strptime error
         date_regex = r'\d{1,2}\s\w{3,9}\s\d{4}'
+
         if release_dates:
-            usa_release_date = [i['date'] for i in release_dates if i['country'] == 'USA\n' and not i.get('notes')]
+
+            usa_release_date = [i['date'] for i in release_dates
+                                if i['country'] == 'USA\n'
+                                and not i.get('notes')]
             if usa_release_date and match(date_regex, usa_release_date[0]):
+
                 release_date = datetime.strptime(usa_release_date[0], '%d %B %Y')
                 if release_date > datetime.now():
-                    db_values = (user_id, user_name, title_id, title_name, None, release_date)
+                # if the title is not out yet, store it in the database
+                    db_values = (user_id, user_name, title_id,
+                                 title_name, None, release_date)
                     message = self.db_api.insert(db_values)
                 else:
                     message = 'Released on {0} in USA'.format(usa_release_date[0])
@@ -121,14 +141,19 @@ class Alert():
             message = 'No release date found'
         return message
 
+
     @_catch_and_log
     def _get_episode_release_date(self, user_id, user_name, title_id, title_name):
         """
         Get release date of next episode and update database if not yet released
-        """
+	"""
+
+        # Get all episodes for title from IMDb
         result = self.imdb_api.get_movie_episodes(title_id)
         title_episodes = result['data'].get('episodes')
+        # regex to check release date string as to avoid datetime.strptime error
         date_regex = r'\d{1,2}\s\w{3}.{0,1}\s\d{4}'
+
         if title_episodes:
             current_season = next(iter(title_episodes))
             latest_episodes = title_episodes[current_season]
@@ -139,85 +164,113 @@ class Alert():
                     release_date = datetime.strptime(ep_release_date, '%d %b %Y')
                     if release_date < datetime.now():
                         if ep_no == len(latest_episodes):
-                            message = 'Season {0} finale aired {1}'.format(current_season, ep_release_date)
+                            message = 'Season {0} finale aired' \
+                                      ' {1}'.format(current_season, ep_release_date)
                             return message
                     else:
+                        # store it in the database
                         title_episode = ep_data.getID()
-                        db_values = (user_id, user_name, title_id, title_name, title_episode, release_date)
+                        db_values = (user_id, user_name, title_id,
+                                     title_name, title_episode, release_date)
                         message = self.db_api.insert(db_values)
                         return message
                 else:
                     message = 'Unable to get episode release date'
         else:
             message = 'Unable to get series episodes'
+
         return message
+
 
     @_catch_and_log
     def _update_episode(self, user_id, title_id, current_episode_data):
         """
         Get next episode ID and release date and update the database
         """
+
         date_regex = r'\d{1,2}\s\w{3}.{0,1}\s\d{4}'
         next_episode_id = current_episode_data.get('next episode')
+
         if next_episode_id:
             next_episode_data = self.imdb_api.get_episode(next_episode_id)
             next_release_date = next_episode_data.get('original air date')
             if next_release_date and match(date_regex, next_release_date):
+            # next episode with valid release date found, store in database
                 next_release_date = next_release_date.replace(',', '')
                 release_date = datetime.strptime(next_release_date, '%d %b %Y')
             else:
+            # no release date for next episode, check again next week
                 next_week_date = datetime.now() + timedelta(days=7)
-                release_date = next_week_date.replace(hour=0, minute=0, second=0, microsecond=0)
+                release_date = next_week_date.replace(hour=0, minute=0,
+                                                      second=0, microsecond=0)
                 next_episode_id = current_episode_data.getID()
+
             db_values = (next_episode_id, release_date, user_id, title_id)
-            self.db_api.update (db_values)
+            self.db_api.update(db_values)
         else:
+            # no next episode not found, assume series ended and remove alert
             self.db_api.delete(user_id, title_id)
+
         return next_episode_id
+
 
     @_catch_and_log
     def enable(self, user_id, user_name, title_id):
         """
         Get movie/series IMDb data and check release date
         """
+
         imdb_data = self.imdb_api.get_movie(title_id, info=('main'))
         title_name = imdb_data.get('long imdb title')
         seasons = imdb_data.get('seasons')
+
         if seasons:
-            message = self._get_episode_release_date(user_id, user_name, title_id, title_name)
+            message = self._get_episode_release_date(user_id, user_name,
+                                                     title_id, title_name)
         else:
-            message = self._get_movie_release_date(user_id, user_name, title_id, title_name)
+            message = self._get_movie_release_date(user_id, user_name,
+                                                   title_id, title_name)
         return message
+
 
     @_catch_and_log
     def disable(self, user_id, title_id):
         """
         Remove user's alert from database
         """
+
         message = self.db_api.delete(user_id, title_id)
         return message
+
 
     @_catch_and_log
     def title_name(self, user_id):
         """
         Return string of user's alert title names from the database
         """
+
         results = self.db_api.query_title_name(user_id)
+
         if not results:
-            message = 'No alerts enabled.\n\nType /help for info on enabling alerts.'
+            message = 'No alerts enabled.\n\n' \
+                      'Type /help for info on enabling alerts.'
         elif isinstance(results, list):
             message = '<b>Alerts enabled for:</b>\n\n' + '\n'.join(results)
         else:
             message = results
+
         return message
+
 
     @_catch_and_log
     def title_id(self, user_id):
         """
         Return list of user's title IDs from the database
         """
+
         results = self.db_api.query_title_id(user_id)
         return results
+
 
     @_catch_and_log
     def notify(self):
@@ -225,9 +278,11 @@ class Alert():
         Update database entry with next episode ID and release date and
         return a list of alerts to send to users
         """
+
         alerts = []
         today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
         rows = self.db_api.query_released(today)
+
         if isinstance(rows, list) and rows:
             for row in rows:
                 user_id, title_id, title_episode_id = row[0], row[1], row[2]
@@ -237,19 +292,24 @@ class Alert():
                     current_release_date = datetime.strptime(current_release, '%d %b %Y')
                     next_episode_id = self._update_episode(user_id, title_id, current_episode)
                     if not next_episode_id:
+                        # no next episode found, disable alert
                         fields = get_fields(current_episode)
                         message = 'Series finale episode!' \
                                   '(alert disabled)\n\n' + reply_message(fields)
                         alerts.append((user_id, message))
                     elif current_release_date == today:
+                        # do not notify multiple times for the same episode as some
+                        # episodes are kept pending their next episode release date
                         fields = get_fields(current_episode)
                         message = 'Episode is out!!\n\n' + reply_message(fields)
                         alerts.append((user_id, message))
                 else:
+                    # movie has been release, disable alert
                     title_data = self.imdb_api.get_movie(title_id)
                     fields = get_fields(title_data)
                     movie_details = reply_message(fields)
                     message = 'Movie is out!\n\n' + movie_details
                     self.db_api.delete(user_id, title_id)
                     alerts.append((user_id, message))
+
         return alerts
